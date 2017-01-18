3e7215677af47e695ea04c3eb4f4172717406c68
Rick Clark <rkingsleyclark@gmail.com>
2017-01-18 16:49:33 +0000

    Added Player test to return correct name

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -3,5 +3,7 @@ require 'player'
 describe Player do
   let(:name) { "Obama" }
   subject(:player) { described_class.new(name: name) }
-  it { is_expected.to respond_to(:name) }
+  it "returns correct name" do
+    expect(player.name).to eq "Obama"
+  end
 end
